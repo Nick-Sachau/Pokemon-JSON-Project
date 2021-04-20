a95279ed122579618ff9e4d36492c72c3eb22836
Connor Kobrigr <cjkobriger@gmail.com>
2021-04-20 13:24:06 -0700

    added abilities

--- Charizard/charizard.js
+++ Charizard/charizard.js
@@ -22,6 +22,8 @@ function createSection(obj) {
 
   let types = obj.types
 
+  let abilities = obj.abilities
+
   const PICTURE = document.createElement('div');
   const NUMBERID = document.createElement('p');
   const IMG = document.createElement('img');
@@ -35,12 +37,16 @@ function createSection(obj) {
   const STATNAMES = document.createElement('div');
   const UL = document.createElement('ul');
   const PROGRESSCONTAINER = document.createElement('div')
+  const ABILITY1 = document.createElement('div')
+  const ABILITY2   = document.createElement('div')
 
   NUMBERID.setAttribute('id', 'numberID');
   PICTURE.setAttribute('id', 'picture');
   IMG.setAttribute('id', 'img');
   IMG.setAttribute('src', '../charizard.png');
   IMG.setAttribute('width', '100%');
+  ABILITY1.setAttribute('id','ability1');
+  ABILITY2.setAttribute('id','ability2');
   TYPE.setAttribute('id', 'type');
   H_W.setAttribute('id', 'h_W');
   HEIGHT.setAttribute('id', 'height');
@@ -56,18 +62,17 @@ function createSection(obj) {
   WEIGHT.textContent = 'Weight: ' + obj.weight;
   NAME.textContent = obj.name
   H3STATS.textContent = 'Base Stats'
+  ABILITY1.textContent = obj.abilities[0].name;
+  ABILITY2.textContent = obj.abilities[1].name;
 
   // goes through all the types and adds it to the div #types
 
   for(type in types) {
+
     let tempType = types[type].type;
-
     const TYPE2 = document.createElement('div');
-
     TYPE2.setAttribute('id', tempType.name);
-
     TYPE2.textContent =  tempType.name
-
     TYPE.appendChild(TYPE2)
   }
 
@@ -103,17 +108,19 @@ function createSection(obj) {
   STATS.appendChild(NAME);
   STATS.appendChild(H3STATS);
   STATS.appendChild(STATNAMES);
+  STATS.appendChild(ABILITY1);
+  STATS.appendChild(ABILITY2);
   STATNAMES.appendChild(UL);
   STATNAMES.appendChild(PROGRESSCONTAINER)
   section.appendChild(PICTURE);
   section.appendChild(STATS);
+
 
   for(let i = 0; i < 6; i++) {
   
     let tempstat = statistics[i].stat;
 
     console.log(statistics[i])  
-
     console.log(tempstat.name)
     
     let myBar = document.createElement('div');
